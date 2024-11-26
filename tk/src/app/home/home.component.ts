c2984d7eb4028ea4aec94da31e7c02778c385bcf
ceomssweb <ceomssweb@gmail.com>
2024-11-26 21:50:46 +0530

    home page modified

--- tk/src/app/home/home.component.ts
+++ tk/src/app/home/home.component.ts
@@ -5,15 +5,18 @@ import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { FlashDetailsComponent } from '../flash-details/flash-details.component';
 import { LoginComponent } from "../login/login.component";
 import { ExamNotificationsComponent } from "../exam-notifications/exam-notifications.component";
+import { TestimonialsComponent } from "../testimonials/testimonials.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainMenuComponent, AnimateOnScrollModule, FlashDetailsComponent, LoginComponent, ExamNotificationsComponent],
+  imports: [MainMenuComponent, AnimateOnScrollModule, TestimonialsComponent, LoginComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  tittle="Thervu Kadal"
 
   constructor() { }
 
--- tk/src/app/testimonials/testimonials.component.ts
+++ tk/src/app/testimonials/testimonials.component.ts
@@ -1,31 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DetailsService } from '../services/UI/home-details/details.service';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
-import { DetailsService } from '../services/UI/home-details/details.service';
-import { Detail } from '../services/UI/home-details/details-enums';
 
 @Component({
-  selector: 'app-flash-details',
-  templateUrl: './flash-details.component.html',
-  styleUrls: ['./flash-details.component.scss'],
+  selector: 'app-testimonials',
+  templateUrl: './testimonials.component.html',
+  styleUrls: ['./testimonials.component.scss'],
   providers: [DetailsService],
   standalone: true,
   imports: [CarouselModule, ButtonModule, TagModule]
 })
-export class FlashDetailsComponent implements OnInit {
+export class TestimonialsComponent implements OnInit {
+
   details:any;
 
   responsiveOptions: any[] | undefined;
   constructor(private detailsService: DetailsService) { }
 
   ngOnInit() {
-    this.details = [
-      {id: 1, description: 'Apple'},
-      {id: 2, description: 'Orange'},
-      {id: 3, description: 'Veg'},
-      {id: 4, description: 'Drinks'}
-    ];
+    this.detailsService.showDetails().then((details) => {
+      this.details = details
+  });
   this.responsiveOptions = [
       {
           breakpoint: '1199px',
