938319e03ce580c69bf0dc9d5f9bd2285ae95805
ceomssweb <ceomssweb@gmail.com>
2024-11-27 22:19:14 +0530

    added UI for login anf input padding reduced to 0.6rem

--- tk/src/app/login/login.component.ts
+++ tk/src/app/login/login.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  imports: [
+    ButtonModule, 
+    InputTextModule,  
+    FormsModule, 
+    ReactiveFormsModule
+  ],
 })
 export class LoginComponent implements OnInit {
+  ifMailReq: boolean = true;
+  ifOTPReq: boolean = false;
+  loginForm: FormGroup | any;
+  value: string | any;
+  passwordValue: string | any;
+    
+    submitted = false;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.loginForm = new FormGroup({
+        'login': new FormControl('', Validators.required),
+        'password': new FormControl('', Validators.required)
+    });
+}
+
+onSubmit() { 
+    this.submitted = true;
+    alert(JSON.stringify(this.loginForm.value));
+}
+showReg(){
+  this.ifMailReq = false;
+  this.ifOTPReq = false;
+  debugger
+}
 
 }
